94b0303048fa14e8cd16d55d42f0fc44dd903b59
nbr <nikolaus.brunner@protonmail.ch>
2025-11-02 15:18:29 +0100

    feat: add open-props for styling system

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import styles from "./index.module.css";
+
 export const Route = createFileRoute("/")({
 	component: App,
 });
 
 function App() {
 	return (
-		<div>
+		<div className={styles.wrapper}>
 			<h1>Black Atom Industries</h1>
 		</div>
 	);
